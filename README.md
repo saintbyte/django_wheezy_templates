c900fd91690cade7b617cc1cb72348663b8bf4a8
SB <admin@openwifi.su>
2019-04-02 02:48:50 +0500

    fix

---
+++ README.md
@@ -0,0 +1,14 @@
+# Django wheezy.templates
+
+### Описание
+
+У вас проблемы с производительностью шаблонов в Django тогда попробуйте этот шаблонизатор 
+
+### Установка
+pip install git+https://github.com/saintbyte/django_wheezy_templates
+
+### Настройка
+
+1. Добавляем в INSTALLED_APPS ( settings.py ) 'django_wheezy_templates'
+
+2. В settings.py добавляем
---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django_wheezy_templates',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',
+    description='Wheezy.template for Django ',
+    long_description=README,
+    url='https://mimobaka.ru/opensource/',
+    author='Yegor',
+    author_email='all@mimobaka.ru',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=2.0',
+        'wheezy.template>=0.1.167',
+    ],
+    zip_safe=False,
+)
